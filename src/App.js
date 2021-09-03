6b267c47e0b3c55b227e112b698595c61ea68601
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-09-03 10:21:51 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <input class="details__userdata" name="details" type="text" placeholder="Фамилия"></input>
+        <input class="details__userdata" name="details" type="text" placeholder="Имя"></input>
+        <input class="details__userdata" name="details" type="text" placeholder="Отчество"></input>
+        <input class="details__userdata" name="details" type="text" placeholder="Телефон"></input>
+        <input class="details__userdata" name="details" type="password" placeholder="Password"></input>
+        <button class="details__submitbutton" type="submit" name="details">JOIN NOW </button>
+      </div>
   );
 }
 
