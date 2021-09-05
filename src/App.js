2394dcef3a456aa5a3b7f4583a657791f63cea43
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-09-05 11:18:47 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+
+
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-        <input class="details__userdata" name="details" type="text" placeholder="Фамилия"></input>
-        <input class="details__userdata" name="details" type="text" placeholder="Имя"></input>
-        <input class="details__userdata" name="details" type="text" placeholder="Отчество"></input>
-        <input class="details__userdata" name="details" type="text" placeholder="Телефон"></input>
-        <input class="details__userdata" name="details" type="password" placeholder="Password"></input>
-        <button class="details__submitbutton" type="submit" name="details">JOIN NOW </button>
+      <form className="details__form" id="auth" action=" https://localhost/form" method="post">
+        <input className="details__name" type="text" placeholder="Фамилия"></input>
+        <input className="details__name" type="text" placeholder="Имя"></input>
+        <input className="details__name" type="text" placeholder="Отчество"></input>
+        <input className="details__userdata" type="text" placeholder="Телефон"></input>
+   
+        
+        <select className="details__select" form="auth" required name="user-country">
+        <option disabled selected>Страна</option>
+        <option value="Таиланд">Таиланд</option>
+        <option value="Индонезия">Индонезия</option>
+        <option value="Малайзия">Малайзия</option>
+        <option value="Вьетнам">Вьетнам</option></select>
+        
+   
+        <button className="details__submitbutton" type="submit">ОТПРАВИТЬ</button>
+        </form>
       </div>
   );
 }
